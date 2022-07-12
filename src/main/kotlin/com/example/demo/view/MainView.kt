354337c3a0fca36369ac913d6a8a718809216df9
Костюков Максим <kostyukov.ms@dvfu.ru>
2022-07-12 23:58:07 +1000

    Added new layout bindings

--- src/main/kotlin/com/example/demo/view/MainView.kt
+++ src/main/kotlin/com/example/demo/view/MainView.kt
@@ -4,6 +4,8 @@ import com.example.demo.app.Styles
 import com.github.doyaaaaaken.kotlincsv.dsl.csvReader
 import javafx.beans.property.SimpleIntegerProperty
 import javafx.beans.property.SimpleStringProperty
+import javafx.collections.FXCollections
+import javafx.collections.ObservableList
 import javafx.scene.paint.Paint
 import javafx.stage.FileChooser
 import tornadofx.*
@@ -19,21 +21,16 @@ class MainView : View("Hello TornadoFX") {
 
     override val root = hbox {
         vbox{
-            controller.colors.onChange {
-                this.children.clear()
-
-                for (i in 0 until controller.colors.size) {
-                    this.children.add(hbox{
-                        label(controller.colors[i].number.value.toString())
-                        colorpicker(mode = ColorPickerMode.MenuButton) {
-                            value = controller.colors[i].color
-                            setOnAction {
-                                controller.colors[i].color = value
-                                println(i.toString() + value)
-                            }
-
+            children.bind(controller.colors) {
+                hbox {
+                    label(it.number.value.toString())
+                    colorpicker(mode = ColorPickerMode.MenuButton) {
+                        value = it.color
+                        setOnAction { _ ->
+                            it.color = value
                         }
-                    })
+
+                    }
                 }
             }
 
@@ -43,6 +40,7 @@ class MainView : View("Hello TornadoFX") {
                controller.openFile(chooseFile("Выберите файл", arrayOf(FileChooser.ExtensionFilter("csv", "*.csv"))))
            }
         }
+
     }
 }
 
@@ -55,15 +53,17 @@ class Color(number: Number, color: String) {
 }
 
 class MyController: Controller() {
-    var colors = mutableListOf<Color>().asObservable()
+    var colors: ObservableList<Color> = FXCollections.observableArrayList()
     var rows: List<List<String>> = listOf()
 
     init {
     }
 
-    fun writeText(value: String) {
-        println("Here is $value")
+    fun IsPowerOf(n: Int, p: Int): Boolean {
+        var log = (Math.log(p.toDouble()) / Math.log(n.toDouble())).toInt()
+        return Math.pow(n.toDouble(), log.toDouble()) == p.toDouble()
     }
+
     fun openFile(files: List<File>) {
         if (files.isEmpty()) {
             println("File was not opened")
@@ -82,12 +82,18 @@ class MyController: Controller() {
                 nums = nums.plusElement(Integer.parseInt(rows[i][j]))
             }
         }
-        var nums_list = nums.toList()
+        var nums_list = nums.toMutableList()
+        // sort nums_list
+        nums_list.sort()
         var temp_colors = mutableListOf<Color>()
         for (x in nums_list.indices) {
             temp_colors.add(Color(nums_list[x], "#000010"))
+            // TODO: Set default colors here
+
         }
         colors.addAll(temp_colors)
         println(min(nums.toList()).toString() + max(nums.toList()).toString())
     }
+
+
 }
